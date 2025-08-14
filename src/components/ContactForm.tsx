65cb89989a2c7011a53f040b39383b72c510f15f
Ishan Saini <ishansaini0105@gmail.com>
2025-08-14 17:09:38 +0530

    Minor UI fix

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -9,7 +9,7 @@ const ContactForm = () => {
           type="text"
           placeholder="Your name"
           name="name"
-          className="w-full px-3 py-2 text-sm text-gray-200 placeholder-gray-400 bg-white border-0 rounded shadow"
+          className="w-full px-3 py-2 text-sm text-black placeholder-gray-400 bg-white border-0 rounded shadow"
         />
       </div>
       <div className="mb-3">
@@ -17,14 +17,14 @@ const ContactForm = () => {
           type="email"
           placeholder="Email"
           name="email"
-          className="w-full px-3 py-2 text-sm text-gray-200 placeholder-gray-400 bg-white border-0 rounded shadow"
+          className="w-full px-3 py-2 text-sm text-black placeholder-gray-400 bg-white border-0 rounded shadow"
         />
       </div>
       <div className="mb-3">
         <input
           placeholder="Your message"
           name="message"
-          className="w-full px-3 py-2 text-sm text-gray-200 placeholder-gray-400 bg-white border-0 rounded shadow"
+          className="w-full px-3 py-2 text-sm text-black placeholder-gray-400 bg-white border-0 rounded shadow"
         />
       </div>
       <button className="px-6 mb-10 py-3 rounded-lg text-sm font-bold text-white uppercase transition-all duration-150 ease-linear bg-blue-500 hover:bg-blue-400">
